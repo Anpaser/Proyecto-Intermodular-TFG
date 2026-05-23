3f5204f9828c047d9b8704d965eba6230e2192b0
Angel <aparedess11@iesarroyoharnina.es>
2026-05-23 17:52:56 +0200

    Commit 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
+    id("org.jetbrains.dokka")
 }
 
 android {
